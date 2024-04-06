f7171d1a4f2e5709da70c03d5caa22815a47d414
Heizer Gábor <hga.posta@hotmail.com>
2024-04-06 14:06:12 +0200

    feat: add default_host_name to output

--- devops/iac/workspaces/live-io-infra/outputs.tf
+++ devops/iac/workspaces/live-io-infra/outputs.tf
@@ -2,3 +2,8 @@ output "duumbi_live_io_rg_id" {
   value       = azurerm_resource_group.duumbi_live_io_rg.id
   description = "identity of duumbi.io resource group"
 }
+
+output "duumbi_live_io_site_swa_default_host_name" {
+  value       = azurerm_static_web_app.duumbi_live_io_site_swa.default_host_name
+  description = "The default host name of the Static Web App"
+}
