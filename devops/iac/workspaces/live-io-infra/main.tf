5f853a2073e9a342a823dee20ea134e0b9bbfd43
Heizer Gábor <hga.posta@hotmail.com>
2024-04-07 18:13:17 +0200

    feat: add env

--- devops/iac/workspaces/live-io-infra/main.tf
+++ devops/iac/workspaces/live-io-infra/main.tf
@@ -3,3 +3,14 @@ resource "azurerm_resource_group" "duumbi_live_io_rg" {
   location = var.location_base.name
   tags     = local.tags
 }
+
+resource "port_entity" "duumbi_live_io_environment_entity" {
+  identifier = var.environment
+  title      = title(var.environment)
+  blueprint  = "environment"
+  properties = {
+    "string_props" = {
+      "name" = var.environment
+    }
+  }
+}
