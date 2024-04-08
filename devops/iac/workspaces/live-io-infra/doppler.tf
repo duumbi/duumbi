37d33b86ad1eaeed9009d8ee087e51f3c7c5661f
Heizer Gábor <hga.posta@hotmail.com>
2024-04-08 15:28:55 +0200

    refactor(iac): remove rg

--- devops/iac/workspaces/live-io-infra/doppler.tf
+++ devops/iac/workspaces/live-io-infra/doppler.tf
@@ -2,3 +2,9 @@ resource "doppler_project" "site_web_application" {
   name        = "site-web-application"
   description = "Official duumbi.io web site"
 }
+
+resource "doppler_environment" "site_web_application_environment" {
+  project = doppler_project.site_web_application.id
+  slug    = var.environment
+  name    = title(var.environment)
+}
--- devops/iac/workspaces/live-io-infra/main.tf
+++ devops/iac/workspaces/live-io-infra/main.tf
@@ -1,10 +1,11 @@
+/*
 resource "azurerm_resource_group" "duumbi_live_io_rg" {
   name     = local.rg_name
   location = var.location_base.name
   tags     = local.tags
 }
 
-/* resource "port_entity" "duumbi_live_io_environment_entity" {
+resource "port_entity" "duumbi_live_io_environment_entity" {
   identifier = var.environment
   title      = title(var.environment)
   blueprint  = "environment"
