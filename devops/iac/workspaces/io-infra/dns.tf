935c018882cc5dee71fa32722eedcc86f7bc77e5
Heizer Gábor <hga.posta@hotmail.com>
2024-04-09 23:18:13 +0200

    feat: add DKIM

--- devops/iac/workspaces/io-infra/dns.tf
+++ devops/iac/workspaces/io-infra/dns.tf
@@ -51,7 +51,7 @@ resource "azurerm_dns_txt_record" "duumbi_io" {
   }
 }
 
-/* resource "azurerm_dns_cname_record" "duumbi_io" {
+resource "azurerm_dns_cname_record" "www_duumbi_io" {
   count = var.environment == "live" ? 1 : 0
 
   name                = "www"
@@ -60,4 +60,19 @@ resource "azurerm_dns_txt_record" "duumbi_io" {
   ttl                 = 300
   record              = azurerm_static_web_app.site_swa.default_host_name
   tags                = local.tags
-} */
+}
+
+resource "azurerm_dns_txt_record" "default_domainkey_duumbi_io" {
+  count = var.environment == "live" ? 1 : 0
+
+  name                = "default._domainkey"
+  zone_name           = local.zone_name
+  resource_group_name = azurerm_resource_group.main_rg.name
+  ttl                 = 1800
+
+  # DKIM key for duumbi.io
+  record {
+    value = "v=DKIM1;k=rsa;p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtYXo1d08UwcX0fTqQKNIKchipsSu82F6DbmgmwPcBkpia3uR664Ra5N6OAtt9lEIGMZprqUVEhgtZKiZd8A98GWFjdypELI6Pju8nq8gqdzowl8o0UlYrjWYMe+fheT6WOFPpAMTDMDt2jlSfocO00N3VJIhINcM93cJyGGYsY8DRmir7VymZyuYKCYbNFTHGu9f1JnHgRKVgppPp7T5il77Cpr//H7sLcpUIRxomug05to4w3cMODDX588veWf+dP4Aymo5K5zrAKJ9r2Ukyd2/YmzHhxtP2kEObZlDUs22SZAjsZsksGMWVNNJdnwH+ry5hdM36jhm0+53jxS4HwIDAQAB"
+  }
+
+}
