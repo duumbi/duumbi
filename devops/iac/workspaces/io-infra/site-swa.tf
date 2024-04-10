b49069fca92de4bb9ab919391262a094458314bb
Heizer Gábor <hga.posta@hotmail.com>
2024-04-10 15:50:56 +0200

    refactor: swa entity

--- devops/iac/workspaces/io-infra/site-swa.tf
+++ devops/iac/workspaces/io-infra/site-swa.tf
@@ -8,25 +8,17 @@ resource "azurerm_static_web_app" "site_swa" {
   app_settings        = {}
 }
 
-data "azurerm_static_web_app" "site_swa" {
-  name                = local.site_swa_name
-  resource_group_name = azurerm_resource_group.main_rg.name
-
-  depends_on = [
-    azurerm_static_web_app.site_swa
-  ]
-}
-
 resource "port_entity" "site_swa_entity" {
   identifier = local.site_swa_name
-  title      = "Duumbi.io Static Web App"
+  title      = "Static Web App - ${local.zone_name}"
   blueprint  = data.terraform_remote_state.idp.outputs.port_azure_static_web_app_identifier
   properties = {
     "string_props" = {
       "name"              = local.site_swa_name
-      "defaultHostName"   = data.azurerm_static_web_app.site_swa.default_host_name
+      "defaultHostName"   = azurerm_static_web_app.site_swa.default_host_name
       "resourceGroupName" = azurerm_resource_group.main_rg.name
       "skuTier"           = azurerm_static_web_app.site_swa.sku_tier
+      "defaultDomain"     = local.zone_name
     }
   }
   relations = {
