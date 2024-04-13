4f020678424b7f2dfa26af4b6042394d53529184
Heizer Gábor <hga.posta@hotmail.com>
2024-04-13 14:51:27 +0200

    fix(idp): set monitor output

--- devops/idp/internal-developer-portal/outputs.tf
+++ devops/idp/internal-developer-portal/outputs.tf
@@ -2,3 +2,13 @@ output "port_azure_static_web_app_identifier" {
   value       = port_blueprint.azure_static_web_app.identifier
   description = "identity of azure static web app"
 }
+
+output "port_uptime_monitor_identifier" {
+  value       = port_blueprint.uptime_monitor.identifier
+  description = "identity of uptime monitor"
+}
+
+output "port_uptime_status_page_identifier" {
+  value       = port_blueprint.uptime_status_page.identifier
+  description = "identity of uptime status page"
+}
