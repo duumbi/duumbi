587202136406741a4fdc71c34d7aaa53fa84a691
Heizer Gábor <hga.posta@hotmail.com>
2024-04-22 10:37:17 +0200

    fix: renovate

--- devops/iac/workspaces/io-auth0/backend.tf
+++ devops/iac/workspaces/io-auth0/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.8.0"
+  required_version = "1.7.0"
 
   cloud {
     hostname     = "app.terraform.io"
@@ -18,7 +18,7 @@ terraform {
 
     port = {
       source  = "port-labs/port-labs"
-      version = "1.10.6"
+      version = "1.10.5"
     }
 
     auth0 = {
