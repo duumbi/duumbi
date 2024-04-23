ff245faa408e52409d5487babc972b2a35bb65d7
Heizer Gábor <hga.posta@hotmail.com>
2024-04-23 16:34:22 +0200

    feat: set output

--- devops/iac/workspaces/io-infra/outputs.tf
+++ devops/iac/workspaces/io-infra/outputs.tf
@@ -2,3 +2,8 @@ output "azurerm_main_rg_id" {
   value       = azurerm_resource_group.main_rg.id
   description = "identity of duumbi.io resource group"
 }
+
+output "azurerm_site_swa_name" {
+  value       = azurerm_static_web_app.site_swa.name
+  description = "name of duumbi.io resource group"
+}
