457600affecd4de4fb89c4be38caa1eda5666f01
Heizer Gábor <hga.posta@hotmail.com>
2024-05-16 14:08:16 +0200

    fix: aks subnet

--- devops/iac/workspaces/io-infra/newtork.tf
+++ devops/iac/workspaces/io-infra/newtork.tf
@@ -10,5 +10,5 @@ resource "azurerm_subnet" "aks" {
   name                 = "aks-subnet"
   resource_group_name  = azurerm_resource_group.main_rg.name
   virtual_network_name = azurerm_virtual_network.main_vnet.name
-  address_prefixes     = var.subnet_address_prefixes["aks"]
+  address_prefixes     = var.subnets_address_prefixes["aks"]
 }
