9ca327641f576d46285f757a6a2f94575df0e441
Heizer Gábor <hga.posta@hotmail.com>
2024-05-20 15:44:32 +0200

    fix: identity

--- devops/iac/workspaces/io-infra/aks.tf
+++ devops/iac/workspaces/io-infra/aks.tf
@@ -1,11 +1,11 @@
 resource "azurerm_user_assigned_identity" "aks_id" {
   name                = "aks-id"
   location            = local.aks_location_name
-  resource_group_name = azurerm_resource_group.main_rg.name
+  resource_group_name = azurerm_resource_group.aks_rg.name
 }
 
 resource "azurerm_role_assignment" "aks_ra" {
-  scope                = azurerm_resource_group.main_rg.id
+  scope                = azurerm_resource_group.aks_rg.id
   role_definition_name = "Network Contributor"
   principal_id         = azurerm_user_assigned_identity.aks_id.principal_id
 }
