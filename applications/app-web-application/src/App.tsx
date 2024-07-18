2378f1890e6017a054f5e417fa4418b82180d3fd
Heizer Gábor <hga.posta@hotmail.com>
2024-07-18 17:49:55 +0200

    fix: base

--- applications/app-web-application/src/App.tsx
+++ applications/app-web-application/src/App.tsx
@@ -1,35 +1,11 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import { Route, Routes } from "react-router-dom";
-import { PageLoader } from "./components/base/page-loader";
-import { AuthenticationGuard } from "./middleware/authentication";
-
-// import Region from "./pages/region";
-import Application from "./pages/application";
-import React from "react";
-// import NotFoundPage from "./pages/not-found-page";
-
-// const Application = React.lazy(() => import("./pages/application"));
-const Region = React.lazy(() => import("./pages/region"));
-const NotFoundPage = React.lazy(() => import("./pages/not-found-page"));
+import Region from "./pages/region";
+import NotFoundPage from "./pages/not-found-page";
 
 function App() {
-  const { isLoading } = useAuth0();
-
-  if (isLoading) {
-    return (
-      <div className="page-layout">
-        <PageLoader />
-      </div>
-    );
-  }
-
   return (
     <Routes>
-      <Route path="/region" element={<Region />} />
-      <Route
-        path="/"
-        element={<AuthenticationGuard component={Application} />}
-      />
+      <Route path="/" element={<Region />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
--- applications/app-web-application/src/main.tsx
+++ applications/app-web-application/src/main.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { Auth0ProviderWithNavigate } from "./auth/auth0-provider-with-navigate.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0ProviderWithNavigate>
-        <App />
-      </Auth0ProviderWithNavigate>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
